7671e497b47c6a2c1710065b45638124bd186ea4
Gupie21 <gupie11722008@hotmail.com>
2022-07-19 23:11:33 -0500

    Portfolio update

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -11,7 +11,7 @@ function Portfolio() {
         </h2>
       </div>
       <div className="portfolio__inner">
-        {itemData.map((item) => {
+        {featureData.map((item) => {
           return (
             <div className="portfolio__card" key={item.img}>
               <div className="portfolio__card-img">
@@ -23,6 +23,10 @@ function Portfolio() {
                 <div className="portfolio__card-content--title">Project</div>
                 <div className="portfolio__card-content--subtitle">{item.title}</div>
                 <div className="portfolio__card-content--description">{item.description}</div>
+                <div className="portfolio-card-content--cta">
+                  <a className="portfolio-cta" href={item.clink} target="_blank" rel="noreferrer"><i className="fa-solid fa-user"></i></a>
+                  <a className="portfolio-cta" href={item.clink} target="_blank" rel="noreferrer"><OpenInNewIcon /></a>
+                </div>
               </div>
             </div>
           );
@@ -32,46 +36,45 @@ function Portfolio() {
   );
 }
 
-const itemData = [
+const featureData = [
   {
     img: "../assets/images/portfolio/IMJUS.jpg",
     title: "IMJUS",
     url: "https://www.behance.net/gallery/148034359/IMJUS",
-    description: "Loren ipsum dolor",
-    tech: "Lorem ipsum",
-    clinks: "Lorem ipsum",
+    description: "Build a site in a Laravel environment with Wordpress that allows people to learn more about this institution, its certifications, seminars and courses through a friendly and intuitive platform. ",
+    tech: "Laravel | Wordpress | AWS Lightsail | Adobe CC",
+    clink: "https://imjus.org.mx/"
   },
   {
     img: "./assets/images/portfolio/Kaikou.jpg",
     title: "Kaikou",
     url: "https://www.behance.net/gallery/148034563/Kaikou",
-    description: "Loren ipsum dolor",
-    tech: "Lorem ipsum",
-    clinks: "Lorem ipsum",
-  },
-  {
-    img: "./assets/images/portfolio/Nexos.jpg",
-    title: "Nexo Integra",
-    url: "@https://www.behance.net/gallery/148041121/Nexo-Integra",
-    description: "Loren ipsum dolor",
-    tech: "Lorem ipsum",
-    clinks: "Lorem ipsum",
+    description: "Build and design a Wordpress multisite site that hosts vendors, sellers and users in a custom marketplace programmed with SCSS, PHP and Javascript.",
+    tech: "Woocommerce | PHP | Javascript | SCSS| AWS Lightsail | Adobe CC",
+    clink: "http://kaikoucbd.com/"
   },
   {
     img: "./assets/images/portfolio/Mabe.jpg",
     title: "Mabe",
     url: "https://www.behance.net/gallery/148040961/Mabe",
-    description: "Loren ipsum dolor",
-    tech: "Lorem ipsum",
-    clinks: "Lorem ipsum",
+    description: "Lead and build a project hand in hand with Mabe's designers a site mounted on SAP Hybris for the sale of products of the brand, taking into account the needs of the company and creating a good UX for end users of this platform.",
+    tech: "SCSS | Javascript | Hybris(Java) | Balsamiq | Adobe CC",
+    clink: "https://mabe.com.mx/"
   },
   {
+    img: "./assets/images/portfolio/Tecnolite.jpg",
+    title: "Tecnolite",
+    url: "https://www.behance.net/gallery/148041407/Tecnolite",
+    description: "Visually improve the platform through end-user testing, taking into account the target market and best practices for the generation of a new UI on your sales site.",
+    tech: "SCSS | Javascript | Hybris(Java) | Balsamiq | Adobe CC",
+    clink: "https://tecnolite.mx/"
+  },
+ /* {
     img: "./assets/images/portfolio/Laud.jpg",
     title: "Laud",
     url: "https://www.behance.net/gallery/148034615/Laud",
     description: "Loren ipsum dolor",
     tech: "Lorem ipsum",
-    clinks: "Lorem ipsum",
   },
   {
     img: "./assets/images/portfolio/MaaTalent.jpg",
@@ -79,7 +82,6 @@ const itemData = [
     url: "https://www.behance.net/gallery/148040887/MAA-Talent",
     description: "Loren ipsum dolor",
     tech: "Lorem ipsum",
-    clinks: "Lorem ipsum",
   },
   {
     img: "./assets/images/portfolio/Totalplay.jpg",
@@ -87,15 +89,6 @@ const itemData = [
     url: "https://www.behance.net/gallery/148041345/Totalplay",
     description: "Loren ipsum dolor",
     tech: "Lorem ipsum",
-    clinks: "Lorem ipsum",
-  },
-  {
-    img: "./assets/images/portfolio/Odontotec.jpg",
-    title: "Odontotec",
-    url: "https://www.behance.net/gallery/148041279/Odontotec",
-    description: "Loren ipsum dolor",
-    tech: "Lorem ipsum",
-    clinks: "Lorem ipsum",
   },
   {
     img: "./assets/images/portfolio/Vicarq.jpg",
@@ -103,7 +96,6 @@ const itemData = [
     url: "https://www.behance.net/gallery/148041439/Vicarq",
     description: "Loren ipsum dolor",
     tech: "Lorem ipsum",
-    clinks: "Lorem ipsum",
   },
   {
     img: "./assets/images/portfolio/Mutuo.jpg",
@@ -111,16 +103,21 @@ const itemData = [
     url: "https://www.behance.net/gallery/148041055/Mutuo-Financiera",
     description: "Loren ipsum dolor",
     tech: "Lorem ipsum",
-    clinks: "Lorem ipsum",
   },
   {
-    img: "./assets/images/portfolio/Tecnolite.jpg",
-    title: "Tecnolite",
-    url: "https://www.behance.net/gallery/148041407/Tecnolite",
+    img: "./assets/images/portfolio/Odontotec.jpg",
+    title: "Odontotec",
+    url: "https://www.behance.net/gallery/148041279/Odontotec",
     description: "Loren ipsum dolor",
     tech: "Lorem ipsum",
-    clinks: "Lorem ipsum",
   },
+  {
+    img: "./assets/images/portfolio/Nexos.jpg",
+    title: "Nexo Integra",
+    url: "@https://www.behance.net/gallery/148041121/Nexo-Integra",
+    description: "Loren ipsum dolor",
+    tech: "Lorem ipsum",
+  },*/
 ];
 
 export { Portfolio };
